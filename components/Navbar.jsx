064cad7d7baf60e8d59727b18f263c3d98a8f67a
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-07-17 19:37:04 +0200

    hero done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,12 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import styles from '../styles';
-import { navVariants } from '../utils/motion';
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
+  // moving elements
   <motion.nav
     variants={navVariants}
     initial="hidden"
@@ -14,14 +15,17 @@ const Navbar = () => (
   >
     <div className="absolute w-[50px] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      {/* log */}
       <img
         src="/search.svg"
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
+      {/* title */}
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
         METAVERSUS
       </h2>
+      {/* hamburger menu */}
       <img
         src="/menu.svg"
         alt="menu"
